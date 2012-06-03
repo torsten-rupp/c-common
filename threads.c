a80e60c699497259c76e8be419df85467d4018b7
Torsten Rupp <torsten.rupp@gmx.net>
2012-06-03 15:29:02 +0000

    added thread local storage variable  functions

--- threads.c
+++ threads.c
@@ -1,12 +1,14 @@
 /***********************************************************************\
 *
-* $Source: /home/torsten/cvs/bar/bar/threads.c,v $
 * $Revision$
+* $Date$
 * $Author$
 * Contents: thread functions
 * Systems: all
 *
 \***********************************************************************/
+
+#define __THREADS_IMPLEMENATION__
 
 /****************************** Includes *******************************/
 #include <stdlib.h>
@@ -17,12 +19,15 @@
 #include <assert.h>
 
 #include "global.h"
+#include "lists.h"
 
 #include "threads.h"
 
 /****************** Conditional compilation switches *******************/
 
 /***************************** Constants *******************************/
+#define THREAD_LOCAL_STOAGE_HASHTABLE_SIZE      15
+#define THREAD_LOCAL_STOAGE_HASHTABLE_INCREMENT 16
 
 /***************************** Datatypes *******************************/
 typedef struct
@@ -45,6 +50,8 @@ typedef struct
 #ifdef __cplusplus
   extern "C" {
 #endif
+
+// ----------------------------------------------------------------------
 
 /***********************************************************************\
 * Name   : threadStart
@@ -93,14 +100,14 @@ bool Thread_init(Thread     *thread,
   assert(name != NULL);
   assert(entryFunction != NULL);
 
-  /* init thread info */
+  // init thread info
   sem_init(&startInfo.lock,0,0);
   startInfo.name          = name;
   startInfo.niceLevel     = niceLevel;
   startInfo.entryFunction = entryFunction;
   startInfo.userData      = userData;
 
-  /* start thread */
+  // start thread
   pthread_attr_init(&threadAttributes);
   #ifdef HAVE_PTHREAD_ATTR_SETNAME
     if (name != NULL)
@@ -111,7 +118,7 @@ bool Thread_init(Thread     *thread,
     UNUSED_VARIABLE(name);
   #endif /* HAVE_PTHREAD_ATTR_SETNAME */
 
-  /* start thread */
+  // start thread
   if (pthread_create(&thread->handle,
                      &threadAttributes,
                      (void*(*)(void*))threadStart,
@@ -122,10 +129,10 @@ bool Thread_init(Thread     *thread,
   }
   pthread_attr_destroy(&threadAttributes);
 
-  /* wait until thread started */
+  // wait until thread started
   sem_wait(&startInfo.lock);
 
-  /* free resources */
+  // free resources
   sem_destroy(&startInfo.lock);
 
   return TRUE;
@@ -145,9 +152,82 @@ void Thread_join(Thread *thread)
   pthread_join(thread->handle,NULL);
 }
 
-void Thread_yield(void)
+void Thread_initLocalVariable(ThreadLocalStorage *threadLocalStorage, ThreadLocalStorageAllocFunction threadLocalStorageAllocFunction, void *threadLocalStorageAllocUserData)
 {
-  sched_yield();
+  assert(threadLocalStorage != NULL);
+
+  threadLocalStorage->allocFunction = threadLocalStorageAllocFunction;
+  threadLocalStorage->allocUserData = threadLocalStorageAllocUserData;
+  pthread_mutex_init(&threadLocalStorage->lock,NULL);
+  List_init(&threadLocalStorage->instanceList);
+}
+
+void Thread_doneLocalVariable(ThreadLocalStorage *threadLocalStorage, ThreadLocalStorageFreeFunction threadLocalStorageFreeFunction, void *threadLocalStorageFreeUserData)
+{
+  ThreadLocalStorageInstanceNode *threadLocalStorageInstanceNode;
+
+  assert(threadLocalStorage != NULL);
+
+  pthread_mutex_lock(&threadLocalStorage->lock);
+  {
+    while (!List_isEmpty(&threadLocalStorage->instanceList))
+    {
+      threadLocalStorageInstanceNode = (ThreadLocalStorageInstanceNode*)List_getFirst(&threadLocalStorage->instanceList);
+      if (threadLocalStorageFreeFunction != NULL) threadLocalStorageFreeFunction(threadLocalStorageInstanceNode->p,threadLocalStorageFreeUserData);
+      LIST_DELETE_NODE(threadLocalStorageInstanceNode);
+    }
+  }
+  pthread_mutex_unlock(&threadLocalStorage->lock);
+}
+
+void *Thread_getLocalVariable(ThreadLocalStorage *threadLocalStorage)
+{
+  ThreadId                       currentThreadId;
+  void                           *p;
+  ThreadLocalStorageInstanceNode *threadLocalStorageInstanceNode;
+
+  assert(threadLocalStorage != NULL);
+
+  pthread_mutex_lock(&threadLocalStorage->lock);
+  {
+    // find instance
+    threadLocalStorageInstanceNode = (ThreadLocalStorageInstanceNode*)List_first(&threadLocalStorage->instanceList);
+    currentThreadId = Thread_getCurrentId();
+    while (   (threadLocalStorageInstanceNode != NULL)
+           && (threadLocalStorageInstanceNode->threadId != currentThreadId)
+          )
+    {
+      threadLocalStorageInstanceNode = threadLocalStorageInstanceNode->next;
+    }
+
+    if (threadLocalStorageInstanceNode == NULL)
+    {
+      // allocate new instance
+      threadLocalStorageInstanceNode = LIST_NEW_NODE(ThreadLocalStorageInstanceNode);
+      if (threadLocalStorageInstanceNode == NULL)
+      {
+        pthread_mutex_unlock(&threadLocalStorage->lock);
+        return NULL;
+      }
+      threadLocalStorageInstanceNode->threadId = currentThreadId;
+
+      threadLocalStorageInstanceNode->p = threadLocalStorage->allocFunction(threadLocalStorage->allocUserData);
+      if (threadLocalStorageInstanceNode->p == NULL)
+      {
+        pthread_mutex_unlock(&threadLocalStorage->lock);
+        LIST_DELETE_NODE(threadLocalStorageInstanceNode);
+        return NULL;
+      }
+
+      // add instance
+      List_append(&threadLocalStorage->instanceList,threadLocalStorageInstanceNode);
+    }
+
+    p = threadLocalStorageInstanceNode->p;
+  }
+  pthread_mutex_unlock(&threadLocalStorage->lock);
+
+  return p;
 }
 
 #ifdef __cplusplus
