79be27f6535319716b7e6caf3a1324e039234a0d
Torsten Rupp <torsten.rupp@gmx.net>
2012-09-29 07:40:50 +0000

    - clean-up - improved error messages

--- cmdoptions.c
+++ cmdoptions.c
@@ -39,7 +39,7 @@ extern "C" {
 * Purpose: get integer option value
 * Input  : value             - value variable
 *          string            - string
-*          option            - option name
+*          name              - option name
 *          units             - units array or NULL
 *          unitCount         - size of unit array
 * Output : value - value
@@ -49,7 +49,7 @@ extern "C" {
 
 LOCAL bool getIntegerOption(int                   *value,
                             const char            *string,
-                            const char            *option,
+                            const char            *name,
                             const CommandLineUnit *units,
                             uint                  unitCount,
                             FILE                  *errorOutputHandle,
@@ -84,7 +84,7 @@ LOCAL bool getIntegerOption(int                   *value,
               "%sValue '%s' for option '%s' is not a number!\n",
               (errorPrefix != NULL) ? errorPrefix : "",
               string,
-              option
+              name
              );
     }
     return FALSE;
@@ -124,8 +124,9 @@ LOCAL bool getIntegerOption(int                   *value,
       if (errorOutputHandle != NULL)
       {
         fprintf(errorOutputHandle,
-                "%sUnexpected unit in value '%s'!\n",
+                "%sUnexpected unit '%s' in value '%s'!\n",
                 (errorPrefix != NULL) ? errorPrefix : "",
+                unit,
                 string
                );
       }
@@ -148,7 +149,7 @@ LOCAL bool getIntegerOption(int                   *value,
 * Purpose: get integer64 option value
 * Input  : value             - value variable
 *          string            - string
-*          option            - option name
+*          name              - option name
 *          units             - units array or NULL
 *          unitCount         - size of unit array
 * Output : value - value
@@ -158,7 +159,7 @@ LOCAL bool getIntegerOption(int                   *value,
 
 LOCAL bool getInteger64Option(int64                 *value,
                               const char            *string,
-                              const char            *option,
+                              const char            *name,
                               const CommandLineUnit *units,
                               uint                  unitCount,
                               FILE                  *errorOutputHandle,
@@ -193,7 +194,7 @@ LOCAL bool getInteger64Option(int64                 *value,
               "%sValue '%s' for option '%s' is not a number!\n",
               (errorPrefix != NULL) ? errorPrefix : "",
               string,
-              option
+              name
              );
     }
     return FALSE;
@@ -233,8 +234,9 @@ LOCAL bool getInteger64Option(int64                 *value,
       if (errorOutputHandle != NULL)
       {
         fprintf(errorOutputHandle,
-                "%sUnexpected unit in value '%s'!\n",
+                "%sUnexpected unit '%s' in value '%s'!\n",
                 (errorPrefix != NULL) ? errorPrefix : "",
+                unit,
                 string
                );
       }
@@ -320,8 +322,8 @@ LOCAL bool processOption(const CommandLineOption *commandLineOption,
         if (!getInteger64Option(commandLineOption->variable.l,
                                 value,
                                 option,
-                                commandLineOption->integerOption.units,
-                                commandLineOption->integerOption.unitCount,
+                                commandLineOption->integer64Option.units,
+                                commandLineOption->integer64Option.unitCount,
                                 errorOutputHandle,
                                 errorPrefix
                                )
@@ -416,8 +418,9 @@ LOCAL bool processOption(const CommandLineOption *commandLineOption,
             if (errorOutputHandle != NULL)
             {
               fprintf(errorOutputHandle,
-                      "%sUnexpected unit in value '%s'!\n",
+                      "%sUnexpected unit '%s' in value '%s'!\n",
                       (errorPrefix != NULL)?errorPrefix:"",
+                      unit,
                       value
                      );
             }
