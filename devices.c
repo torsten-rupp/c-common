3840cfd740b4421a608d934f76f4e92b2bfa4092
Torsten Rupp <torsten.rupp@gmx.net>
2013-04-07 07:21:33 +0000

    support WIN32

--- devices.c
+++ devices.c
@@ -18,12 +18,17 @@
 #include <dirent.h>
 #include <utime.h>
 #include <fcntl.h>
-#include <sys/ioctl.h>
+#ifdef HAVE_SYS_IOCTL_H
+  #include <sys/ioctl.h>
+#endif
 #include <errno.h>
 #include <assert.h>
 
-#include <linux/types.h>
-#include <linux/fs.h>
+#if   defined(PLATFORM_LINUX)
+  #include <linux/types.h>
+  #include <linux/fs.h>
+#elif defined(PLATFORM_WINDOWS)
+#endif /* PLATFORM_... */
 
 #include "global.h"
 #include "strings.h"
@@ -42,6 +47,41 @@
 /***************************** Variables *******************************/
 
 /****************************** Macros *********************************/
+#ifdef HAVE_FSEEKO
+  #define FSEEK fseeko
+#elif HAVE__FSEEKI64
+  #define FSEEK _fseeki64
+#else
+  #define FSEEK fseek
+#endif
+
+#ifdef HAVE_FTELLO
+  #define FTELL ftello
+#elif HAVE__FTELLI64
+  #define FTELL _ftelli64
+#else
+  #define FTELL ftell
+#endif
+
+#ifdef HAVE_STAT64
+  #define STAT  stat64
+  #define LSTAT lstat64
+  typedef struct stat64 FileStat;
+#elif HAVE___STAT64
+  #define STAT  stat64
+  #define LSTAT lstat64
+  typedef struct __stat64 FileStat;
+#elif HAVE__STATI64
+  #define STAT  _stati64
+  #define LSTAT _stati64
+  typedef struct _stati64 FileStat;
+#elif HAVE_STAT
+  #define STAT  stat
+  #define LSTAT lstat
+  typedef struct stat FileStat;
+#else
+  #error No struct stat64 nor struct __stat64
+#endif
 
 /***************************** Forwards ********************************/
 
@@ -71,14 +111,14 @@ Errors Device_open(DeviceHandle *deviceHandle,
       deviceHandle->file = fopen(String_cString(deviceName),"rb");
       if (deviceHandle->file == NULL)
       {
-        return ERRORX(OPEN_DEVICE,errno,String_cString(deviceName));
+        return ERRORX_(OPEN_DEVICE,errno,String_cString(deviceName));
       }
       break;
     case DEVICE_OPEN_WRITE:
       deviceHandle->file = fopen(String_cString(deviceName),"r+b");
       if (deviceHandle->file == NULL)
       {
-        return ERRORX(OPEN_DEVICE,errno,String_cString(deviceName));
+        return ERRORX_(OPEN_DEVICE,errno,String_cString(deviceName));
       }
       break;
     #ifndef NDEBUG
@@ -89,22 +129,22 @@ Errors Device_open(DeviceHandle *deviceHandle,
   }
 
   // get device size
-  if (fseeko(deviceHandle->file,(off_t)0,SEEK_END) == -1)
+  if (FSEEK(deviceHandle->file,(off_t)0,SEEK_END) == -1)
   {
-    error = ERRORX(IO_ERROR,errno,String_cString(deviceName));
+    error = ERRORX_(IO_ERROR,errno,String_cString(deviceName));
     fclose(deviceHandle->file);
     return error;
   }
-  n = ftello(deviceHandle->file);
+  n = FTELL(deviceHandle->file);
   if (n == (off_t)(-1))
   {
-    error = ERRORX(IO_ERROR,errno,String_cString(deviceName));
+    error = ERRORX_(IO_ERROR,errno,String_cString(deviceName));
     fclose(deviceHandle->file);
     return error;
   }
-  if (fseeko(deviceHandle->file,(off_t)0,SEEK_SET) == -1)
+  if (FSEEK(deviceHandle->file,(off_t)0,SEEK_SET) == -1)
   {
-    error = ERRORX(IO_ERROR,errno,String_cString(deviceName));
+    error = ERRORX_(IO_ERROR,errno,String_cString(deviceName));
     fclose(deviceHandle->file);
     return error;
   }
@@ -169,7 +209,7 @@ Errors Device_read(DeviceHandle *deviceHandle,
       || ((n < (ssize_t)bufferLength) && (bytesRead == NULL))
      )
   {
-    return ERRORX(IO_ERROR,errno,String_cString(deviceHandle->name));
+    return ERRORX_(IO_ERROR,errno,String_cString(deviceHandle->name));
   }
   deviceHandle->index += n;
 
@@ -194,7 +234,7 @@ Errors Device_write(DeviceHandle *deviceHandle,
   if (deviceHandle->index > deviceHandle->size) deviceHandle->size = deviceHandle->index;
   if (n != (ssize_t)bufferLength)
   {
-    return ERRORX(IO_ERROR,errno,String_cString(deviceHandle->name));
+    return ERRORX_(IO_ERROR,errno,String_cString(deviceHandle->name));
   }
 
   return ERROR_NONE;
@@ -215,10 +255,10 @@ Errors Device_tell(DeviceHandle *deviceHandle, uint64 *offset)
   assert(deviceHandle->file != NULL);
   assert(offset != NULL);
 
-  n = ftello(deviceHandle->file);
+  n = FTELL(deviceHandle->file);
   if (n == (off_t)(-1))
   {
-    return ERRORX(IO_ERROR,errno,String_cString(deviceHandle->name));
+    return ERRORX_(IO_ERROR,errno,String_cString(deviceHandle->name));
   }
 // NYI
 //assert(sizeof(off_t)==8);
@@ -236,9 +276,9 @@ Errors Device_seek(DeviceHandle *deviceHandle,
   assert(deviceHandle != NULL);
   assert(deviceHandle->file != NULL);
 
-  if (fseeko(deviceHandle->file,(off_t)offset,SEEK_SET) == -1)
+  if (FSEEK(deviceHandle->file,(off_t)offset,SEEK_SET) == -1)
   {
-    return ERRORX(IO_ERROR,errno,String_cString(deviceHandle->name));
+    return ERRORX_(IO_ERROR,errno,String_cString(deviceHandle->name));
   }
   deviceHandle->index = offset;
 
@@ -255,7 +295,7 @@ Errors Device_openDeviceList(DeviceListHandle *deviceListHandle)
   deviceListHandle->file = fopen("/proc/partitions","r");
   if (deviceListHandle->file == NULL)
   {
-    return ERROR(OPEN_FILE,errno);
+    return ERROR_(OPEN_FILE,errno);
   }
 
   // skip first line (header line)
@@ -364,7 +404,7 @@ Errors Device_readDeviceList(DeviceListHandle *deviceListHandle,
     // parse and get device name
     if (!String_scanCString(deviceListHandle->line,"%* %* %* %256s %*",buffer))
     {
-      return ERRORX(IO_ERROR,0,"invalid format");
+      return ERRORX_(IO_ERROR,0,"invalid format");
     }
     String_setCString(deviceName,"/dev");
     File_appendFileNameCString(deviceName,buffer);
@@ -384,10 +424,14 @@ Errors Device_getDeviceInfo(DeviceInfo   *deviceInfo,
                             const String deviceName
                            )
 {
-  struct stat64 fileStat;
-  int           handle;
-  int           i;
-  long          l;
+  FileStat fileStat;
+  int      handle;
+  #if defined(HAVE_IOCTL) && defined(HAVE_BLKSSZGET)
+    int      i;
+  #endif
+  #if defined(HAVE_IOCTL) && defined(HAVE_BLKGETSIZE)
+    long     l;
+  #endif
 
   assert(deviceName != NULL);
   assert(deviceInfo != NULL);
@@ -401,7 +445,7 @@ Errors Device_getDeviceInfo(DeviceInfo   *deviceInfo,
 //  deviceInfo->mountedFlag = FALSE;
 
   // get device meta data
-  if (lstat64(String_cString(deviceName),&fileStat) == 0)
+  if (LSTAT(String_cString(deviceName),&fileStat) == 0)
   {
     deviceInfo->timeLastAccess  = fileStat.st_atime;
     deviceInfo->timeModified    = fileStat.st_mtime;
@@ -409,8 +453,16 @@ Errors Device_getDeviceInfo(DeviceInfo   *deviceInfo,
     deviceInfo->userId          = fileStat.st_uid;
     deviceInfo->groupId         = fileStat.st_gid;
     deviceInfo->permission      = (DevicePermission)fileStat.st_mode;
-    deviceInfo->major           = major(fileStat.st_rdev);
-    deviceInfo->minor           = minor(fileStat.st_rdev);
+    #ifdef HAVE_MAJOR
+      deviceInfo->major         = major(fileStat.st_rdev);
+    #else
+      deviceInfo->major         = 0;
+    #endif
+    #ifdef HAVE_MINOR
+      deviceInfo->minor         = minor(fileStat.st_rdev);
+    #else
+      deviceInfo->minor         = 0;
+    #endif
     deviceInfo->id              = (uint64)fileStat.st_ino;
 
     if      (S_ISCHR(fileStat.st_mode)) deviceInfo->type = DEVICE_TYPE_CHARACTER;
@@ -423,13 +475,17 @@ Errors Device_getDeviceInfo(DeviceInfo   *deviceInfo,
     handle = open(String_cString(deviceName),O_RDONLY);
     if (handle != -1)
     {
-      if (ioctl(handle,BLKSSZGET, &i) == 0) deviceInfo->blockSize = (ulong)i;
-      if (ioctl(handle,BLKGETSIZE,&l) == 0) deviceInfo->size      = (int64)l*512;
+      #if defined(HAVE_IOCTL) && defined(HAVE_BLKSSZGET)
+        if (ioctl(handle,BLKSSZGET, &i) == 0) deviceInfo->blockSize = (ulong)i;
+      #endif
+      #if defined(HAVE_IOCTL) && defined(HAVE_BLKGETSIZE)
+        if (ioctl(handle,BLKGETSIZE,&l) == 0) deviceInfo->size      = (int64)l*512;
+      #endif
       close(handle);
     }
     else
     {
-      return ERRORX(OPEN_DEVICE,errno,String_cString(deviceName));
+      return ERRORX_(OPEN_DEVICE,errno,String_cString(deviceName));
     }
   }
 
