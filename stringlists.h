3ce38fcedad0460bac54935ee3fadce7d7d7904a
Torsten Rupp <torsten.rupp@gmx.net>
2013-07-28 03:34:57 +0000

    - implemented StringList_initDuplicate() - added from/to node in StringList_copy(), StringList_move() - return list in StringList_clear()

--- stringlists.h
+++ stringlists.h
@@ -12,7 +12,7 @@
 #define __STRING_LISTS__
 
 /****************************** Includes *******************************/
-#include <config.h>  // use <...> to support separated build directory 
+#include <config.h>  // use <...> to support separated build directory
 
 #include <stdlib.h>
 #include <stdio.h>
@@ -70,9 +70,12 @@ typedef struct
 * Return : -
 * Notes  : variable will contain all strings in list
 *          usage:
+*            StringNode *iteratorVariable;
+*            String     variable;
+*
 *            STRINGLIST_ITERATE(list,iteratorVariable,variable)
 *            {
-*              ... = variable->...
+*              ... = variable
 *            }
 \***********************************************************************/
 
@@ -92,7 +95,7 @@ typedef struct
 
 /***********************************************************************\
 * Name   : StringList_init
-* Purpose: initialise string list
+* Purpose: initialize string list
 * Input  : stringList - string list to initialize
 * Output : -
 * Return : -
@@ -100,6 +103,18 @@ typedef struct
 \***********************************************************************/
 
 void StringList_init(StringList *stringList);
+
+/***********************************************************************\
+* Name   : StringList_initDuplicate
+* Purpose: initialize duplicated string list
+* Input  : stringList     - string list to initialize
+*          fromStringList - string list to copy (strings will be copied!)
+* Output : -
+* Return : -
+* Notes  : -
+\***********************************************************************/
+
+void StringList_initDuplicate(StringList *stringList, const StringList *fromStringList);
 
 /***********************************************************************\
 * Name   : StringList_done
@@ -137,7 +152,7 @@ StringList *StringList_duplicate(const StringList *stringList);
 /***********************************************************************\
 * Name   : StringList_copy
 * Purpose: copy sting list
-* Input  : stringList - string list
+* Input  : stringList     - string list
 *          fromStringList - string list to copy (strings will be copied!)
 * Output : -
 * Return : string list or NULL on insufficient memory
@@ -162,11 +177,11 @@ void StringList_delete(StringList *stringList);
 * Purpose: remove all entry in list
 * Input  : stringList - string list
 * Output : -
-* Return : -
-* Notes  :
+* Return : string list
+* Notes  : -
 \***********************************************************************/
 
-void StringList_clear(StringList *stringList);
+StringList *StringList_clear(StringList *stringList);
 
 /***********************************************************************\
 * Name   : StringList_move
