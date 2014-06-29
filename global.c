337c090ac0b98d4b8ae5ab02effa3eb4864baaee
Torsten Rupp <torsten.rupp@gmx.net>
2014-06-29 09:58:27 +0000

    added dprintf()

--- global.c
+++ global.c
@@ -106,6 +106,22 @@ LOCAL void debugResourceInit(void)
 
 // ----------------------------------------------------------------------
 
+#ifndef NDEBUG
+void __dprintf__(const char *__fileName__,
+                 uint       __lineNb__,
+                 const char *format,
+                 ...
+                )
+{
+  va_list arguments;
+
+  va_start(arguments,format);
+  vfprintf(stdout,format,arguments);
+  va_end(arguments);
+  fprintf(stdout,"\n");
+}
+#endif /* NDEBUG */
+
 void __halt(const char *__fileName__,
             uint       __lineNb__,
             int        exitcode,
@@ -666,7 +682,7 @@ void debugDumpCurrentStackTrace(FILE *handle, const char *title, uint indent)
 #endif /* !defined(NDEBUG) && defined(HAVE_BACKTRACE) */
 
 #ifndef NDEBUG
-void debugDumpMemory(bool printAddress, const void *address, uint length)
+void debugDumpMemory(const void *address, uint length, bool printAddress)
 {
   const byte *p;
   uint       z,i;
