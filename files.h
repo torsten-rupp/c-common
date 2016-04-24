323345a893ab24cb2a25f62c2b29fffaa0c9e42d
Torsten Rupp <torsten.rupp@gmx.net>
2016-04-24 09:34:16 +0000

    fixed debug code

--- files.h
+++ files.h
@@ -321,14 +321,14 @@ typedef struct
 /****************************** Macros *********************************/
 
 #ifndef NDEBUG
-  #define File_getTmpFile(...)             __File_getTmpFile(__FILE__,__LINE__,__VA_ARGS__)
-  #define File_getTmpFileCString(...)      __File_getTmpFileCString(__FILE__,__LINE__,__VA_ARGS__)
-  #define File_open(...)                   __File_open(__FILE__,__LINE__,__VA_ARGS__)
-  #define File_openCString(...)            __File_openCString(__FILE__,__LINE__,__VA_ARGS__)
-  #define File_openDescriptor(...)         __File_openDescriptor(__FILE__,__LINE__,__VA_ARGS__)
-  #define File_close(...)                  __File_close(__FILE__,__LINE__,__VA_ARGS__)
-  #define File_initExtendedAttributes(...) __File_initExtendedAttributes(__FILE__,__LINE__,__VA_ARGS__)
-  #define File_doneExtendedAttributes(...) __File_doneExtendedAttributes(__FILE__,__LINE__,__VA_ARGS__)
+  #define File_getTmpFile(...)             __File_getTmpFile(__FILE__,__LINE__, ## __VA_ARGS__)
+  #define File_getTmpFileCString(...)      __File_getTmpFileCString(__FILE__,__LINE__, ## __VA_ARGS__)
+  #define File_open(...)                   __File_open(__FILE__,__LINE__, ## __VA_ARGS__)
+  #define File_openCString(...)            __File_openCString(__FILE__,__LINE__, ## __VA_ARGS__)
+  #define File_openDescriptor(...)         __File_openDescriptor(__FILE__,__LINE__, ## __VA_ARGS__)
+  #define File_close(...)                  __File_close(__FILE__,__LINE__, ## __VA_ARGS__)
+  #define File_initExtendedAttributes(...) __File_initExtendedAttributes(__FILE__,__LINE__, ## __VA_ARGS__)
+  #define File_doneExtendedAttributes(...) __File_doneExtendedAttributes(__FILE__,__LINE__, ## __VA_ARGS__)
 #endif /* not NDEBUG */
 
 /***************************** Forwards ********************************/
@@ -574,7 +574,7 @@ const char *File_getSystemTmpDirectory(void);
 \***********************************************************************/
 
 Errors File_getTmpFileName(String fileName, ConstString pattern, ConstString directory);
-Errors File_getTmpFileNameCString(String fileName, const char *pattern, ConstString directory);
+Errors File_getTmpFileNameCString(String fileName, const char *pattern, const char *directory);
 
 /***********************************************************************\
 * Name   : File_getTmpDirectoryName, File_getTmpDirectoryNameCString
