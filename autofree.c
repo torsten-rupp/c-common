98f651e8883262b58b5bf309b2fd24c0f854a6f7
Torsten Rupp <torsten.rupp@gmx.net>
2017-11-11 13:06:36 +0000

    fixed warning

--- autofree.c
+++ autofree.c
@@ -195,7 +195,6 @@ void __AutoFree_remove(const char   *__fileName__,
     if (!foundFlag)
     {
       #ifndef NDEBUG
-        fprintf(stderr,"DEBUG WARNING: auto resource %p not found in auto resource list at %s, line %u\n",
         fprintf(stderr,"DEBUG WARNING: auto resource %p not found in auto resource list at %s, line %lu\n",
                 resource,
                 __fileName__,
