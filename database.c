63fd8bfc6d358df50f71dac590bdf84afde74af6
Torsten Rupp <torsten.rupp@gmx.net>
2017-11-25 11:46:36 +0000

    fixed checkpoints

--- database.c
+++ database.c
@@ -49,6 +49,8 @@
 
 /***************************** Constants *******************************/
 #define MAX_FORCE_CHECKPOINT_TIME (10LL*60LL*1000LL) // timeout for force execution of a checkpoint [ms]
+//#define CHECKPOINT_MODE           SQLITE_CHECKPOINT_RESTART
+#define CHECKPOINT_MODE           SQLITE_CHECKPOINT_TRUNCATE
 
 #if 1
   #define DEBUG_WARNING_LOCK_TIME  2ULL*1000ULL      // DEBUG only: warning lock time [ms]
@@ -861,7 +863,7 @@ LOCAL void executeCheckpoint(DatabaseHandle *databaseHandle)
 
   if (Misc_getTimestamp() > (databaseHandle->lastCheckpointTimestamp+MAX_FORCE_CHECKPOINT_TIME*US_PER_MS))
   {
-    (void)sqlite3_wal_checkpoint_v2(databaseHandle->handle,NULL,SQLITE_CHECKPOINT_RESTART,NULL,NULL);
+    (void)sqlite3_wal_checkpoint_v2(databaseHandle->handle,NULL,CHECKPOINT_MODE,NULL,NULL);
     databaseHandle->lastCheckpointTimestamp = Misc_getTimestamp();
   }
 }
@@ -2364,7 +2366,7 @@ Errors Database_copyTable(DatabaseHandle                *fromDatabaseHandle,
         }
         return error;
       }
-      lastRowId = (uint64)sqlite3_last_insert_rowid(toDatabaseHandle->handle);
+      lastRowId = (DatabaseId)sqlite3_last_insert_rowid(toDatabaseHandle->handle);
       LIST_ITERATE(&toColumnList,columnNode)
       {
         switch (columnNode->type)
@@ -4466,12 +4468,12 @@ Errors Database_vsetString(DatabaseHandle *databaseHandle,
   return ERROR_NONE;
 }
 
-int64 Database_getLastRowId(DatabaseHandle *databaseHandle)
+DatabaseId Database_getLastRowId(DatabaseHandle *databaseHandle)
 {
   assert(databaseHandle != NULL);
   assert(databaseHandle->handle != NULL);
 
-  return (uint64)sqlite3_last_insert_rowid(databaseHandle->handle);
+  return (DatabaseId)sqlite3_last_insert_rowid(databaseHandle->handle);
 }
 
 #ifndef NDEBUG
