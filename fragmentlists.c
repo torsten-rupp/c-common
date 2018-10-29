024167c30f18a0089ac07ce1b26a358568ab285e
Torsten Rupp <torsten.rupp@gmx.net>
2018-10-29 18:09:41 +0000

    fragment lists

---
+++ fragmentlists.c
@@ -0,0 +1,377 @@
+/***********************************************************************\
+*
+* $Revision: 8580 $
+* $Date: 2018-07-07 09:20:56 +0200 (Sat, 07 Jul 2018) $
+* $Author: torsten $
+* Contents: Backup ARchiver fragment list functions
+* Systems: all
+*
+\***********************************************************************/
+
+/****************************** Includes *******************************/
+#include <stdlib.h>
+#include <stdio.h>
+#include <inttypes.h>
+#include <assert.h>
+
+#include "common/global.h"
+#include "common/lists.h"
+#include "common/strings.h"
+
+#include "fragmentlists.h"
+
+/****************** Conditional compilation switches *******************/
+
+/***************************** Constants *******************************/
+
+/***************************** Datatypes *******************************/
+
+/***************************** Variables *******************************/
+
+/****************************** Macros *********************************/
+
+// get begin/end index i0,i1
+#define I0(offset,length) (offset)
+#define I1(offset,length) (((length)>0)?(offset)+(length)-1:(offset))
+
+// begin/end index f0,f1 of fragment
+#define F0(fragmentEntryNode) I0(fragmentEntryNode->offset,fragmentEntryNode->length)
+#define F1(fragmentEntryNode) I1(fragmentEntryNode->offset,fragmentEntryNode->length)
+
+/***************************** Forwards ********************************/
+
+/***************************** Functions *******************************/
+
+#ifdef __cplusplus
+  extern "C" {
+#endif
+
+/***********************************************************************\
+* Name   : freeFragmentNode
+* Purpose: free fragment node
+* Input  : fragmentNode - fragment node
+* Output : -
+* Return : -
+* Notes  : -
+\***********************************************************************/
+
+LOCAL void freeFragmentNode(FragmentNode *fragmentNode, void *userData)
+{
+  assert(fragmentNode != NULL);
+
+  UNUSED_VARIABLE(userData);
+
+  List_done(&fragmentNode->fragmentEntryList,NULL,NULL);
+  if (fragmentNode->userData != NULL)
+  {
+    free(fragmentNode->userData);
+  }
+  String_delete(fragmentNode->name);
+}
+
+/***********************************************************************\
+* Name   : printSpaces
+* Purpose: print spaces
+* Input  : outputHandle - output file handle
+*          n            - number of spaces to print
+* Output : -
+* Return : -
+* Notes  : -
+\***********************************************************************/
+
+LOCAL void printSpaces(FILE *outputHandle, uint n)
+{
+  const char *SPACES8 = "        ";
+
+  uint z;
+
+  assert(outputHandle != NULL);
+
+  z = 0;
+  while ((z+8) < n)
+  {
+    (void)fwrite(SPACES8,1,8,outputHandle);
+    z += 8;
+  }
+  while (z < n)
+  {
+    (void)fputc(' ',outputHandle);
+    z++;
+  }
+}
+
+/*---------------------------------------------------------------------*/
+
+void FragmentList_init(FragmentList *fragmentList)
+{
+  assert(fragmentList != NULL);
+
+  List_init(fragmentList);
+  Semaphore_init(&fragmentList->lock,SEMAPHORE_TYPE_BINARY);
+}
+
+void FragmentList_done(FragmentList *fragmentList)
+{
+  assert(fragmentList != NULL);
+
+  Semaphore_done(&fragmentList->lock);
+  List_done(fragmentList,(ListNodeFreeFunction)freeFragmentNode,NULL);
+}
+
+void FragmentList_initNode(FragmentNode *fragmentNode,
+                           ConstString  name,
+                           uint64       size,
+                           const void   *userData,
+                           uint         userDataSize
+                          )
+{
+  assert(fragmentNode != NULL);
+
+  fragmentNode->name = String_duplicate(name);
+  fragmentNode->size = size;
+  if (userData != NULL)
+  {
+    fragmentNode->userData = malloc(userDataSize);
+    if (fragmentNode->userData == NULL)
+    {
+      HALT_INSUFFICIENT_MEMORY();
+    }
+    memcpy(fragmentNode->userData,userData,userDataSize);
+    fragmentNode->userDataSize = userDataSize;
+  }
+  else
+  {
+    fragmentNode->userData     = NULL;
+    fragmentNode->userDataSize = 0;
+  }
+  List_init(&fragmentNode->fragmentEntryList);
+}
+
+void FragmentList_doneNode(FragmentNode *fragmentNode)
+{
+  assert(fragmentNode != NULL);
+
+  freeFragmentNode(fragmentNode,NULL);
+}
+
+FragmentNode *FragmentList_add(FragmentList *fragmentList,
+                               ConstString  name,
+                               uint64       size,
+                               const void   *userData,
+                               uint         userDataSize
+                              )
+{
+  FragmentNode *fragmentNode;
+
+  assert(fragmentList != NULL);
+  assert(name != NULL);
+
+  fragmentNode = LIST_NEW_NODE(FragmentNode);
+  if (fragmentNode == NULL)
+  {
+    HALT_INSUFFICIENT_MEMORY();
+  }
+  FragmentList_initNode(fragmentNode,name,size,userData,userDataSize);
+
+  List_append(fragmentList,fragmentNode);
+
+  return fragmentNode;
+}
+
+void FragmentList_discard(FragmentList *fragmentList, FragmentNode *fragmentNode)
+{
+  assert(fragmentList != NULL);
+  assert(fragmentNode != NULL);
+
+  List_remove(fragmentList,fragmentNode);
+  FragmentList_doneNode(fragmentNode);
+  LIST_DELETE_NODE(fragmentNode);
+}
+
+FragmentNode *FragmentList_find(const FragmentList *fragmentList, ConstString name)
+{
+  FragmentNode *fragmentNode;
+
+  assert(fragmentList != NULL);
+  assert(name != NULL);
+
+  return LIST_FIND(fragmentList,fragmentNode,String_equals(fragmentNode->name,name));
+}
+
+void FragmentList_clearEntry(FragmentNode *fragmentNode)
+{
+  assert(fragmentNode != NULL);
+
+  List_done(&fragmentNode->fragmentEntryList,NULL,NULL);
+}
+
+void FragmentList_addEntry(FragmentNode *fragmentNode,
+                           uint64       offset,
+                           uint64       length
+                          )
+{
+  FragmentEntryNode *fragmentEntryNode,*deleteFragmentEntryNode;
+  FragmentEntryNode *prevFragmentEntryNode,*nextFragmentEntryNode;
+
+  assert(fragmentNode != NULL);
+
+  // remove all fragments which are completely covered by new fragment
+  fragmentEntryNode = fragmentNode->fragmentEntryList.head;
+  while (fragmentEntryNode != NULL)
+  {
+    if ((F0(fragmentEntryNode) >= I0(offset,length)) && (F1(fragmentEntryNode) <= I1(offset,length)))
+    {
+      deleteFragmentEntryNode = fragmentEntryNode;
+      fragmentEntryNode = fragmentEntryNode->next;
+      List_remove(&fragmentNode->fragmentEntryList,deleteFragmentEntryNode);
+      LIST_DELETE_NODE(deleteFragmentEntryNode);
+    }
+    else
+    {
+      fragmentEntryNode = fragmentEntryNode->next;
+    }
+  }
+
+  // find prev/next fragment
+  prevFragmentEntryNode = NULL;
+  fragmentEntryNode = fragmentNode->fragmentEntryList.head;
+  while ((fragmentEntryNode != NULL) && (F1(fragmentEntryNode) <= I1(offset,length)))
+  {
+    prevFragmentEntryNode = fragmentEntryNode;
+    fragmentEntryNode = fragmentEntryNode->next;
+  }
+  nextFragmentEntryNode = NULL;
+  fragmentEntryNode = fragmentNode->fragmentEntryList.tail;
+  while ((fragmentEntryNode != NULL) && (F0(fragmentEntryNode) >= I0(offset,length)))
+  {
+    nextFragmentEntryNode = fragmentEntryNode;
+    fragmentEntryNode = fragmentEntryNode->prev;
+  }
+
+  // check if existing fragment can be extended or new fragment have to be inserted
+  if (   ((prevFragmentEntryNode != NULL) && (F1(prevFragmentEntryNode)+1 >= I0(offset,length)))
+      || ((nextFragmentEntryNode != NULL) && (I1(offset,length)+1 >= F0(nextFragmentEntryNode)))
+     )
+  {
+    if      ((prevFragmentEntryNode != NULL) && (F1(prevFragmentEntryNode)+1 >= I0(offset,length)))
+    {
+      // combine with previous existing fragment
+      prevFragmentEntryNode->length = (offset+length)-prevFragmentEntryNode->offset;
+      prevFragmentEntryNode->offset = prevFragmentEntryNode->offset;
+    }
+    else if ((nextFragmentEntryNode != NULL) && (I1(offset,length)+1 >= F0(nextFragmentEntryNode)))
+    {
+      // combine with next existing fragment
+      nextFragmentEntryNode->length = (nextFragmentEntryNode->offset+nextFragmentEntryNode->length)-offset;
+      nextFragmentEntryNode->offset = offset;
+    }
+
+    if ((prevFragmentEntryNode != NULL) && (nextFragmentEntryNode != NULL) && (F1(prevFragmentEntryNode)+1 >= F0(nextFragmentEntryNode)))
+    {
+      // combine previous and next fragment
+      prevFragmentEntryNode->length += nextFragmentEntryNode->length;
+      List_remove(&fragmentNode->fragmentEntryList,nextFragmentEntryNode);
+      LIST_DELETE_NODE(nextFragmentEntryNode);
+    }
+  }
+  else
+//  else if (   ((prevFragmentEntryNode == NULL) || (F1(prevFragmentEntryNode)+1 < I0(offset,length)))
+//           && ((nextFragmentEntryNode == NULL) || (F0(nextFragmentEntryNode)-1 > I1(offset,length)))
+//          )
+  {
+    // insert new fragment
+    fragmentEntryNode = LIST_NEW_NODE(FragmentEntryNode);
+    if (fragmentEntryNode == NULL)
+    {
+      HALT_INSUFFICIENT_MEMORY();
+    }
+    fragmentEntryNode->offset = offset;
+    fragmentEntryNode->length = length;
+    List_insert(&fragmentNode->fragmentEntryList,fragmentEntryNode,nextFragmentEntryNode);
+  }
+}
+
+bool FragmentList_entryExists(const FragmentNode *fragmentNode,
+                              uint64             offset,
+                              uint64             length
+                             )
+{
+  bool              existsFlag;
+  uint64            i0,i1;
+  FragmentEntryNode *fragmentEntryNode;
+
+  assert(fragmentNode != NULL);
+
+  i0 = I0(offset,length);
+  i1 = I1(offset,length);
+
+  existsFlag = FALSE;
+  for (fragmentEntryNode = fragmentNode->fragmentEntryList.head; (fragmentEntryNode != NULL) && !existsFlag; fragmentEntryNode = fragmentEntryNode->next)
+  {
+    if (   ((F0(fragmentEntryNode) <= i0) && (i0 <= F1(fragmentEntryNode)) )
+        || ((F0(fragmentEntryNode) <= i1) && (i1 <= F1(fragmentEntryNode)))
+       )
+    {
+      existsFlag = TRUE;
+    }
+  }
+
+  return existsFlag;
+}
+
+bool FragmentList_isEntryComplete(const FragmentNode *fragmentNode)
+{
+  assert(fragmentNode != NULL);
+
+  return    (fragmentNode->size == 0)
+         || (   (List_count(&fragmentNode->fragmentEntryList) == 1)
+             && (fragmentNode->fragmentEntryList.head->offset == 0)
+             && (fragmentNode->fragmentEntryList.head->length >= fragmentNode->size)
+            );
+}
+
+void FragmentList_print(FILE               *outputHandle,
+                        uint               indent,
+                        const FragmentNode *fragmentNode,
+                        bool               printMissingFlag
+                       )
+{
+  FragmentEntryNode *fragmentEntryNode;
+  uint64            offset0,offset1;
+  uint64            lastOffset;
+
+  lastOffset = 0LL;
+  LIST_ITERATE(&fragmentNode->fragmentEntryList,fragmentEntryNode)
+//  for (fragmentEntryNode = fragmentNode->fragmentEntryList.head; fragmentEntryNode != NULL; fragmentEntryNode = fragmentEntryNode->next)
+  {
+    offset0 = F0(fragmentEntryNode);
+    offset1 = F1(fragmentEntryNode);
+    if (printMissingFlag)
+    {
+      if ((lastOffset+1) < offset0)
+      {
+        printSpaces(outputHandle,indent); fprintf(outputHandle,"%15"PRIu64"..%15"PRIu64" %15"PRIu64" bytes: missing\n",lastOffset,offset0-1,offset0-lastOffset+1);
+      }
+      printSpaces(outputHandle,indent); fprintf(outputHandle,"%15"PRIu64"..%15"PRIu64" %15"PRIu64" bytes: OK\n",offset0,offset1,offset1-offset0+1);
+      lastOffset = offset1;
+    }
+    else
+    {
+      printSpaces(outputHandle,indent); fprintf(outputHandle,"%15"PRIu64"..%15"PRIu64" %15"PRIu64" bytes\n",offset0,offset1,offset1-offset0+1);
+    }
+  }
+}
+
+#ifndef NDEBUG
+void FragmentList_debugPrintInfo(const FragmentNode *fragmentNode, const char *name)
+{
+  fprintf(stdout,"Fragments '%s':\n",name);
+  FragmentList_print(stdout,0,fragmentNode,FALSE);
+}
+#endif /* not NDEBUG */
+
+#ifdef __cplusplus
+  }
+#endif
+
+/* end of file */
---
+++ fragmentlists.h
@@ -0,0 +1,306 @@
+/***********************************************************************\
+*
+* $Revision: 8581 $
+* $Date: 2018-07-07 09:21:04 +0200 (Sat, 07 Jul 2018) $
+* $Author: torsten $
+* Contents: Backup ARchiver fragment list functions
+* Systems: all
+*
+\***********************************************************************/
+
+#ifndef __FRAGMENT_LISTS__
+#define __FRAGMENT_LISTS__
+
+/****************************** Includes *******************************/
+#include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
+
+#include "common/global.h"
+#include "common/lists.h"
+#include "common/strings.h"
+#include "semaphores.h"
+
+/****************** Conditional compilation switches *******************/
+
+/***************************** Constants *******************************/
+
+/***************************** Datatypes *******************************/
+
+// fragment list entry node
+typedef struct FragmentEntryNode
+{
+  LIST_NODE_HEADER(struct FragmentEntryNode);
+
+  uint64 offset;                        // fragment offset (0..n)
+  uint64 length;                        // length of fragment
+} FragmentEntryNode;
+
+// fragment list entry
+typedef struct
+{
+  LIST_HEADER(FragmentEntryNode);
+} FragmentEntryList;
+
+// fragment list node
+typedef struct FragmentNode
+{
+  LIST_NODE_HEADER(struct FragmentNode);
+
+  String            name;               // fragment name
+  uint64            size;               // size of fragment
+  void              *userData;
+  uint              userDataSize;
+  FragmentEntryList fragmentEntryList;
+} FragmentNode;
+
+// sorted fragment list
+typedef struct
+{
+  LIST_HEADER(FragmentNode);
+
+  Semaphore lock;
+} FragmentList;
+
+/***************************** Variables *******************************/
+
+/****************************** Macros *********************************/
+
+/***********************************************************************\
+* Name   : FRAGMENTLIST_ITERATE
+* Purpose: iterated over fragment list and execute block
+* Input  : fragmentList - fragment list
+*          variable     - iterator variable (type FragmentNode)
+* Output : -
+* Return : -
+* Notes  : variable will contain all entries in list
+*            FRAGMENTLIST_ITERATE(list,variable)
+*            {
+*              ... = variable->...
+*            }
+\***********************************************************************/
+
+#define FRAGMENTLIST_ITERATE(fragmentList,variable) \
+  for ((variable) = (fragmentList)->head; \
+       (variable) != NULL; \
+       (variable) = (variable)->next \
+      )
+
+/***********************************************************************\
+* Name   : FRAGMENTLIST_ITERATEX
+* Purpose: iterated over fragment list and execute block
+* Input  : fragmentList - fragment list
+*          variable     - iterator variable (type FragmentNode)
+*          condition    - additional condition
+* Output : -
+* Return : -
+* Notes  : variable will contain all entries in list
+*            FRAGMENTLIST_ITERATEX(list,variable,TRUE)
+*            {
+*              ... = variable->...
+*            }
+\***********************************************************************/
+
+#define FRAGMENTLIST_ITERATEX(fragmentList,variable,condition) \
+  for ((variable) = (fragmentList)->head; \
+       ((variable) != NULL) && (condition); \
+       (variable) = (variable)->next \
+      )
+
+/***************************** Forwards ********************************/
+
+/***************************** Functions *******************************/
+
+#ifdef __cplusplus
+  extern "C" {
+#endif
+
+/***********************************************************************\
+* Name   : FragmentList_init
+* Purpose: init fragment list
+* Input  : fragmentList - fragment list
+* Output : fragmentList - initialize fragment list
+* Return : -
+* Notes  : -
+\***********************************************************************/
+
+void FragmentList_init(FragmentList *fragmentList);
+
+/***********************************************************************\
+* Name   : FragmentList_done
+* Purpose: free all nodes and deinitialize fragment list
+* Input  : fragmentList - fragment list
+* Output : -
+* Return : -
+* Notes  : -
+\***********************************************************************/
+
+void FragmentList_done(FragmentList *fragmentList);
+
+/***********************************************************************\
+* Name   : FragmentList_initNode
+* Purpose: init fragment node
+* Input  : name         - name of fragment
+*          size         - size of fragment
+*          userData     - user data to store with fragment (will be
+*                         copied!)
+*          userDataSize - size of user data
+* Output : -
+* Return : -
+* Notes  : -
+\***********************************************************************/
+
+void FragmentList_initNode(FragmentNode *fragmentNode,
+                           ConstString  name,
+                           uint64       size,
+                           const void   *userData,
+                           uint         userDataSize
+                          );
+
+/***********************************************************************\
+* Name   : FragmentList_doneNode
+* Purpose: free fragment node
+* Input  : fragmentNode - fragment node
+* Output : -
+* Return : -
+* Notes  : -
+\***********************************************************************/
+
+void FragmentList_doneNode(FragmentNode *fragmentNode);
+
+/***********************************************************************\
+* Name   : FragmentList_add
+* Purpose: add new fragment to fragment list
+* Input  : fragmentList - fragment list
+*          name         - name of fragment
+*          size         - size of fragment
+*          userData     - user data to store with fragment (will be
+*                         copied!)
+*          userDataSize - size of user data
+* Output : -
+* Return : fragment node or NULL on error
+* Notes  :
+\***********************************************************************/
+
+FragmentNode *FragmentList_add(FragmentList *fragmentList,
+                               ConstString  name,
+                               uint64       size,
+                               const void   *userData,
+                               uint         userDataSize
+                              );
+
+/***********************************************************************\
+* Name   : FragmentList_discard
+* Purpose: remove fragment from fragment list and free all resources
+* Input  : fragmentList - fragment list
+*          fragmentNode - fragment node
+* Output : -
+* Return : -
+* Notes  : -
+\***********************************************************************/
+
+void FragmentList_discard(FragmentList *fragmentList, FragmentNode *fragmentNode);
+
+/***********************************************************************\
+* Name   : FragmentList_find
+* Purpose: find fragment in fragment list
+* Input  : fragmentList - fragment list
+*          name         - name
+* Output : -
+* Return : -
+* Notes  : -
+\***********************************************************************/
+
+FragmentNode *FragmentList_find(const FragmentList *fragmentList, ConstString name);
+
+/***********************************************************************\
+* Name   : FragmentList_clearEntry
+* Purpose: clear all fragment entries of fragment
+* Input  : fragmentNode - fragment node to clear
+* Output : -
+* Return : -
+* Notes  : -
+\***********************************************************************/
+
+void FragmentList_clearEntry(FragmentNode *fragmentNode);
+
+/***********************************************************************\
+* Name   : FragmentList_addEntry
+* Purpose: add a fragment entry to a fragment
+* Input  : fragmentNode - fragment node
+*          offset       - fragment offset (0..n)
+*          length       - length of fragment
+* Output : -
+* Return : -
+* Notes  : -
+\***********************************************************************/
+
+void FragmentList_addEntry(FragmentNode *fragmentNode,
+                           uint64       offset,
+                           uint64       length
+                          );
+
+/***********************************************************************\
+* Name   : FragmentList_entryExists
+* Purpose: check if fragment entry already exists in fragment
+* Input  : fragmentNode - fragment node
+*          offset       - fragment offset (0..n)
+*          length       - length of fragment
+* Output : -
+* Return : -
+* Notes  : -
+\***********************************************************************/
+
+bool FragmentList_entryExists(const FragmentNode *fragmentNode,
+                              uint64             offset,
+                              uint64             length
+                             );
+
+/***********************************************************************\
+* Name   : FragmentList_isEntryComplete
+* Purpose: check if fragment is completed (no fragmentation)
+* Input  : fragmentNode - fragment node
+* Output : -
+* Return : TRUE if fragment completed (no fragmented), FALSE otherwise
+* Notes  : -
+\***********************************************************************/
+
+bool FragmentList_isEntryComplete(const FragmentNode *fragmentNode);
+
+/***********************************************************************\
+* Name   : FragmentList_print
+* Purpose: print fragment entries of fragment
+* Input  : outputHandle - output handle
+*          indent       - indention
+*          fragmentNode - fragment node
+* Output : -
+* Return : -
+* Notes  : -
+\***********************************************************************/
+
+void FragmentList_print(FILE               *outputHandle,
+                        uint               indent,
+                        const FragmentNode *fragmentNode,
+                        bool               printMissingFlag
+                       );
+
+#ifndef NDEBUG
+/***********************************************************************\
+* Name   : FragmentList_debugPrintInfo
+* Purpose: fragment list debug function: output info to fragment entries
+* Input  : -
+* Output : -
+* Return : -
+* Notes  : -
+\***********************************************************************/
+
+void FragmentList_debugPrintInfo(const FragmentNode *fragmentNode, const char *name);
+#endif /* not NDEBUG */
+
+#ifdef __cplusplus
+  }
+#endif
+
+#endif /* __FRAGMENT_LISTS__ */
+
+/* end of file */
