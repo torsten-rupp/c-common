c1122a86a8e1404cac0b75f704e31c24c127bbc0
Torsten Rupp <torsten.rupp@gmx.net>
2019-09-30 18:27:13 +0000

    clean-up

--- common/misc.c
+++ common/misc.c
@@ -1510,7 +1510,7 @@ Errors Misc_executeCommand(const char        *commandTemplate,
   uint            n,z;
 
   error = ERROR_NONE;
-  if (commandTemplate != NULL)
+  if (!stringIsEmpty(commandTemplate))
   {
     commandLine = String_new();
     name        = String_new();
@@ -1678,7 +1678,7 @@ Errors Misc_executeScript(const char        *script,
                     arguments,
                     script,
                     CALLBACK(stdoutExecuteIOFunction,stdoutExecuteIOUserData),
-                    0,
+                    0,  // stdoutStripCount
                     CALLBACK(stderrExecuteIOFunction,stderrExecuteIOUserData),
                     String_length(tmpFileName)+1+1
                    );
