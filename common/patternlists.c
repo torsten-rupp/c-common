10584422e0513024ce02171c6ae66f1615a1982f
Torsten Rupp <torsten.rupp@gmx.net>
2021-06-06 10:34:19 +0000

    patterns

---
+++ common/patternlists.c
@@ -0,0 +1,359 @@
+/***********************************************************************\
+*
+* $Revision: 10541 $
+* $Date: 2020-03-02 10:33:09 +0100 (Mon, 02 Mar 2020) $
+* $Author: torsten $
+* Contents: Backup ARchiver pattern list functions
+* Systems: all
+*
+\***********************************************************************/
+
+/****************************** Includes *******************************/
+#include <config.h>  // use <...> to support separated build directory
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#if defined(HAVE_PCRE)
+  #include <pcreposix.h>
+#elif defined(HAVE_REGEX_H)
+  #include <regex.h>
+#else
+  #error No regular expression library available!
+#endif /* HAVE_PCRE || HAVE_REGEX_H */
+#include <assert.h>
+
+#include "common/global.h"
+#include "common/lists.h"
+#include "common/misc.h"
+#include "common/strings.h"
+#include "common/stringlists.h"
+#include "common/patterns.h"
+
+#include "patternlists.h"
+
+/****************** Conditional compilation switches *******************/
+
+/***************************** Constants *******************************/
+
+/***************************** Datatypes *******************************/
+
+/***************************** Variables *******************************/
+
+/****************************** Macros *********************************/
+
+/***************************** Forwards ********************************/
+
+/***************************** Functions *******************************/
+
+#ifdef __cplusplus
+  extern "C" {
+#endif
+
+/***********************************************************************\
+* Name   : duplicatePatternNode
+* Purpose: duplicate pattern node
+* Input  : patternNode - pattern node
+* Output : -
+* Return : copied pattern node
+* Notes  : -
+\***********************************************************************/
+
+LOCAL PatternNode *duplicatePatternNode(PatternNode *patternNode,
+                                        void        *userData
+                                       )
+{
+  PatternNode *newPatternNode;
+  Errors      error;
+
+  assert(patternNode != NULL);
+
+  UNUSED_VARIABLE(userData);
+
+  // allocate pattern node
+  newPatternNode = LIST_NEW_NODE(PatternNode);
+  if (newPatternNode == NULL)
+  {
+    HALT_INSUFFICIENT_MEMORY();
+  }
+  newPatternNode->id     = Misc_getId();
+  newPatternNode->string = String_duplicate(patternNode->string);
+
+  // create pattern
+  error = Pattern_copy(&newPatternNode->pattern,
+                       &patternNode->pattern
+                      );
+  if (error != ERROR_NONE)
+  {
+    String_delete(newPatternNode->string);
+    LIST_DELETE_NODE(newPatternNode);
+    return NULL;
+  }
+
+  return newPatternNode;
+}
+
+/***********************************************************************\
+* Name   : freePatternNode
+* Purpose: free allocated pattern node
+* Input  : patternNode - pattern node
+* Output : -
+* Return : -
+* Notes  : -
+\***********************************************************************/
+
+LOCAL void freePatternNode(PatternNode *patternNode,
+                           void        *userData
+                          )
+{
+  assert(patternNode != NULL);
+  assert(patternNode->string != NULL);
+
+  UNUSED_VARIABLE(userData);
+
+  Pattern_done(&patternNode->pattern);
+  String_delete(patternNode->string);
+}
+
+/*---------------------------------------------------------------------*/
+
+Errors PatternList_initAll(void)
+{
+  return ERROR_NONE;
+}
+
+void PatternList_doneAll(void)
+{
+}
+
+void PatternList_init(PatternList *patternList)
+{
+  assert(patternList != NULL);
+
+  List_init(patternList);
+}
+
+void PatternList_initDuplicate(PatternList       *patternList,
+                               const PatternList *fromPatternList,
+                               const PatternNode *fromPatternListFromNode,
+                               const PatternNode *fromPatternListToNode
+                              )
+{
+  assert(patternList != NULL);
+  assert(fromPatternList != NULL);
+
+  PatternList_init(patternList);
+  PatternList_copy(patternList,fromPatternList,fromPatternListFromNode,fromPatternListToNode);
+}
+
+void PatternList_done(PatternList *patternList)
+{
+  assert(patternList != NULL);
+
+  List_done(patternList,(ListNodeFreeFunction)freePatternNode,NULL);
+}
+
+PatternList *PatternList_clear(PatternList *patternList)
+{
+  assert(patternList != NULL);
+
+  return (PatternList*)List_clear(patternList,(ListNodeFreeFunction)freePatternNode,NULL);
+}
+
+void PatternList_copy(PatternList       *toPatternList,
+                      const PatternList *fromPatternList,
+                      const PatternNode *fromPatternListFromNode,
+                      const PatternNode *fromPatternListToNode
+                     )
+{
+  assert(toPatternList != NULL);
+  assert(fromPatternList != NULL);
+
+  List_copy(toPatternList,NULL,fromPatternList,fromPatternListFromNode,fromPatternListToNode,(ListNodeDuplicateFunction)duplicatePatternNode,NULL);
+}
+
+void PatternList_move(PatternList       *toPatternList,
+                      PatternList       *fromPatternList,
+                      const PatternNode *fromPatternListFromNode,
+                      const PatternNode *fromPatternListToNode
+                     )
+{
+  assert(fromPatternList != NULL);
+  assert(toPatternList != NULL);
+
+  List_move(toPatternList,NULL,fromPatternList,fromPatternListFromNode,fromPatternListToNode);
+}
+
+Errors PatternList_append(PatternList  *patternList,
+                          ConstString  string,
+                          PatternTypes patternType,
+                          uint         *id
+                         )
+{
+  assert(patternList != NULL);
+  assert(string != NULL);
+
+  return PatternList_appendCString(patternList,String_cString(string),patternType,id);
+}
+
+Errors PatternList_appendCString(PatternList  *patternList,
+                                 const char   *string,
+                                 PatternTypes patternType,
+                                 uint         *id
+                                )
+{
+  PatternNode *patternNode;
+  Errors      error;
+
+  assert(patternList != NULL);
+  assert(string != NULL);
+
+  // allocate pattern node
+  patternNode = LIST_NEW_NODE(PatternNode);
+  if (patternNode == NULL)
+  {
+    HALT_INSUFFICIENT_MEMORY();
+  }
+  patternNode->id     = Misc_getId();
+  patternNode->string = String_newCString(string);
+
+  // init pattern
+  error = Pattern_initCString(&patternNode->pattern,
+                              string,
+                              patternType,
+                              PATTERN_FLAG_IGNORE_CASE
+                             );
+  if (error != ERROR_NONE)
+  {
+    String_delete(patternNode->string);
+    LIST_DELETE_NODE(patternNode);
+    return error;
+  }
+
+  // add to list
+  List_append(patternList,patternNode);
+
+  if (id != NULL) (*id) = patternNode->id;
+
+  return ERROR_NONE;
+}
+
+Errors PatternList_update(PatternList  *patternList,
+                          uint         id,
+                          ConstString  string,
+                          PatternTypes patternType
+                         )
+{
+  assert(patternList != NULL);
+  assert(string != NULL);
+
+  return PatternList_updateCString(patternList,id,String_cString(string),patternType);
+}
+
+Errors PatternList_updateCString(PatternList  *patternList,
+                                 uint         id,
+                                 const char   *string,
+                                 PatternTypes patternType
+                                )
+{
+  PatternNode *patternNode;
+  Pattern     pattern;
+  Errors      error;
+
+  assert(patternList != NULL);
+  assert(string != NULL);
+
+  // find pattern node
+  patternNode = (PatternNode*)LIST_FIND(patternList,patternNode,patternNode->id == id);
+  if (patternNode != NULL)
+  {
+    // compile pattern
+    error = Pattern_initCString(&pattern,
+                                string,
+                                patternType,
+                                PATTERN_FLAG_IGNORE_CASE
+                               );
+    if (error != ERROR_NONE)
+    {
+      return error;
+    }
+
+    // store
+    String_setCString(patternNode->string,string);
+    Pattern_done(&patternNode->pattern);
+    patternNode->pattern     = pattern;
+  }
+
+  return ERROR_NONE;
+}
+
+bool PatternList_remove(PatternList *patternList,
+                        uint        id
+                       )
+{
+  PatternNode *patternNode;
+
+  assert(patternList != NULL);
+
+  patternNode = (PatternNode*)LIST_FIND(patternList,patternNode,patternNode->id == id);
+  if (patternNode != NULL)
+  {
+    List_removeAndFree(patternList,patternNode,(ListNodeFreeFunction)freePatternNode,NULL);
+    return TRUE;
+  }
+  else
+  {
+    return FALSE;
+  }
+}
+
+bool PatternList_match(const PatternList *patternList,
+                       ConstString       string,
+                       PatternMatchModes patternMatchMode
+                      )
+{
+  bool        matchFlag;
+  PatternNode *patternNode;
+
+  assert(patternList != NULL);
+  assert(string != NULL);
+
+  matchFlag = FALSE;
+  patternNode = patternList->head;
+  while ((patternNode != NULL) && !matchFlag)
+  {
+    matchFlag = Pattern_match(&patternNode->pattern,string,patternMatchMode);
+    patternNode = patternNode->next;
+  }
+
+  return matchFlag;
+}
+
+bool PatternList_matchStringList(const PatternList *patternList,
+                                 const StringList  *stringList,
+                                 PatternMatchModes patternMatchMode
+                                )
+{
+  bool       matchFlag;
+  StringNode *stringNode;
+
+  assert(patternList != NULL);
+  assert(stringList != NULL);
+
+  matchFlag  = FALSE;
+  stringNode = stringList->head;
+  while ((stringNode != NULL) && !matchFlag)
+  {
+    matchFlag = PatternList_match(patternList,stringNode->string,patternMatchMode);
+    stringNode = stringNode->next;
+  }
+
+  return matchFlag;
+}
+
+#ifdef __cplusplus
+  }
+#endif
+
+/* end of file */
---
+++ common/patternlists.h
@@ -0,0 +1,305 @@
+/***********************************************************************\
+*
+* $Revision: 9251 $
+* $Date: 2019-02-09 10:22:29 +0100 (Sat, 09 Feb 2019) $
+* $Author: torsten $
+* Contents: Backup ARchiver pattern list functions
+* Systems: all
+*
+\***********************************************************************/
+
+#ifndef __PATTERNLISTS__
+#define __PATTERNLISTS__
+
+/****************************** Includes *******************************/
+#include <config.h>  // use <...> to support separated build directory
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/types.h>
+#if defined(HAVE_PCRE)
+  #include <pcreposix.h>
+#elif defined(HAVE_REGEX_H)
+  #include <regex.h>
+#else
+  #error No regular expression library available!
+#endif /* HAVE_PCRE || HAVE_REGEX_H */
+#include <assert.h>
+
+#include "common/global.h"
+#include "lists.h"
+#include "strings.h"
+#include "stringlists.h"
+#include "patterns.h"
+
+#include "errors.h"
+
+/****************** Conditional compilation switches *******************/
+
+/***************************** Constants *******************************/
+
+/***************************** Datatypes *******************************/
+
+typedef struct PatternNode
+{
+  LIST_NODE_HEADER(struct PatternNode);
+
+  uint         id;               // unique node id
+  String       string;           // pattern string
+  Pattern      pattern;          // pattern
+} PatternNode;
+
+typedef struct
+{
+  LIST_HEADER(PatternNode);
+} PatternList;
+
+/***************************** Variables *******************************/
+
+/****************************** Macros *********************************/
+
+/***********************************************************************\
+* Name   : PATTERNLIST_ITERATE
+* Purpose: iterated over pattern list and execute block
+* Input  : list     - list
+*          variable - iterator variable (type PatternNode)
+* Output : -
+* Return : -
+* Notes  : usage:
+*            PATTERNLIST_ITERATE(list,variable)
+*            {
+*              ... = variable->...
+*            }
+\***********************************************************************/
+
+#define PATTERNLIST_ITERATE(list,variable) \
+  for ((variable) = (list)->head; \
+       (variable) != NULL; \
+       (variable) = (variable)->next \
+      )
+
+/***********************************************************************\
+* Name   : PATTERNLIST_ITERATEX
+* Purpose: iterated over pattern list and execute block
+* Input  : list      - list
+*          variable  - iterator variable (type PatternNode)
+*          condition - additional condition
+* Output : -
+* Return : -
+* Notes  : usage:
+*            PATTERNLIST_ITERATEX(list,variable,TRUE)
+*            {
+*              ... = variable->...
+*            }
+\***********************************************************************/
+
+#define PATTERNLIST_ITERATEX(list,variable,condition) \
+  for ((variable) = (list)->head; \
+       ((variable) != NULL) && (condition); \
+       (variable) = (variable)->next \
+      )
+
+/***************************** Forwards ********************************/
+
+/***************************** Functions *******************************/
+
+#ifdef __cplusplus
+  extern "C" {
+#endif
+
+/***********************************************************************\
+* Name   : PatternList_initAll
+* Purpose: init pattern lists
+* Input  : -
+* Output : -
+* Return : ERROR_NONE or errorcode
+* Notes  : -
+\***********************************************************************/
+
+Errors PatternList_initAll(void);
+
+/***********************************************************************\
+* Name   : PatternList_doneAll
+* Purpose: deinitialize pattern lists
+* Input  : -
+* Output : -
+* Return : ERROR_NONE or errorcode
+* Notes  : -
+\***********************************************************************/
+
+void PatternList_doneAll(void);
+
+/***********************************************************************\
+* Name   : PatternList_init
+* Purpose: init pattern list
+* Input  : patternList - pattern list
+* Output : -
+* Return : -
+* Notes  : -
+\***********************************************************************/
+
+void PatternList_init(PatternList *patternList);
+
+/***********************************************************************\
+* Name   : PatternList_initDuplicate
+* Purpose: init duplicated pattern list
+* Input  : patternList             - pattern list
+*          fromPatternList         - from pattern list (source)
+*          fromPatternListFromNode - from node (could be NULL)
+*          fromPatternListToNode   - to node (could be NULL)
+* Output : -
+* Return : -
+* Notes  : -
+\***********************************************************************/
+
+void PatternList_initDuplicate(PatternList       *patternList,
+                               const PatternList *fromPatternList,
+                               const PatternNode *fromPatternListFromNode,
+                               const PatternNode *fromPatternListToNode
+                              );
+
+/***********************************************************************\
+* Name   : PatternList_done
+* Purpose: done pattern list
+* Input  : patternList - pattern list
+* Output : -
+* Return : -
+* Notes  : -
+\***********************************************************************/
+
+void PatternList_done(PatternList *patternList);
+
+/***********************************************************************\
+* Name   : PatternList_clear
+* Purpose: remove all patterns in list
+* Input  : patternList - pattern list
+* Output : -
+* Return : pattern list
+* Notes  : -
+\***********************************************************************/
+
+PatternList *PatternList_clear(PatternList *patternList);
+
+/***********************************************************************\
+* Name   : Pattern_copyList
+* Purpose: copy all patterns from source list to destination list
+* Input  : toPatternList           - to pattern list (destination)
+*          fromPatternList         - from pattern list (source)
+*          fromPatternListFromNode - from node (could be NULL)
+*          fromPatternListToNode   - to node (could be NULL)
+* Output : -
+* Return : -
+* Notes  : -
+\***********************************************************************/
+
+void PatternList_copy(PatternList       *toPatternList,
+                      const PatternList *fromPatternList,
+                      const PatternNode *fromPatternListFromNode,
+                      const PatternNode *fromPatternListToNode
+                     );
+
+/***********************************************************************\
+* Name   : PatternList_move
+* Purpose: move all patterns from source list to destination list
+* Input  : toPatternList           - to pattern list (destination)
+*          fromPatternList         - from pattern list (source)
+*          fromPatternListFromNode - from node (could be NULL)
+*          fromPatternListToNode   - to node (could be NULL)
+* Output : -
+* Return : -
+* Notes  : -
+\***********************************************************************/
+
+void PatternList_move(PatternList       *toPatternList,
+                      PatternList       *fromPatternList,
+                      const PatternNode *fromPatternListFromNode,
+                      const PatternNode *fromPatternListToNode
+                     );
+
+/***********************************************************************\
+* Name   : PatternList_append, PatternList_appendCString
+* Purpose: add pattern to pattern list
+* Input  : patternList - pattern list
+*          string      - pattern
+*          patternType - pattern type; see PATTERN_TTYPE_*
+* Output : id - pattern node id (can be NULL)
+* Return : ERROR_NONE or error code
+* Notes  : -
+\***********************************************************************/
+
+Errors PatternList_append(PatternList  *patternList,
+                          ConstString  string,
+                          PatternTypes patternType,
+                          uint         *id
+                         );
+Errors PatternList_appendCString(PatternList  *patternList,
+                                 const char   *string,
+                                 PatternTypes patternType,
+                                 uint         *id
+                                );
+
+/***********************************************************************\
+* Name   : PatternList_update, PatternList_updateCString
+* Purpose: update pattern in pattern list
+* Input  : patternList - pattern list
+*          id          - pattern node id
+*          string      - pattern
+*          patternType - pattern type; see PATTERN_TTYPE_*
+* Output : -
+* Return : ERROR_NONE or error code
+* Notes  : -
+\***********************************************************************/
+
+Errors PatternList_update(PatternList  *patternList,
+                          uint         id,
+                          ConstString  string,
+                          PatternTypes patternType
+                         );
+Errors PatternList_updateCString(PatternList  *patternList,
+                                 uint         id,
+                                 const char   *string,
+                                 PatternTypes patternType
+                                );
+
+/***********************************************************************\
+* Name   : PatternList_remove
+* Purpose: remove pattern from pattern list
+* Input  : patternList - pattern list
+*          id          - pattern node id
+* Output : -
+* Return : TRUE iff removed
+* Notes  : -
+\***********************************************************************/
+
+bool PatternList_remove(PatternList *patternList,
+                        uint        id
+                       );
+
+/***********************************************************************\
+* Name   : PatternList_match, PatternList_matchStringList
+* Purpose: patch string with all patterns of list
+* Input  : patternList      - pattern list
+*          string           - string
+*          stringList       - string list
+*          patternMatchMode - pattern match mode; see PatternMatchModes
+* Output : -
+* Return : TRUE if pattern match, FALSE otherwise
+* Notes  : -
+\***********************************************************************/
+
+bool PatternList_match(const PatternList *patternList,
+                       ConstString       string,
+                       PatternMatchModes patternMatchMode
+                      );
+bool PatternList_matchStringList(const PatternList *patternList,
+                                 const StringList  *stringList,
+                                 PatternMatchModes patternMatchMode
+                                );
+
+#ifdef __cplusplus
+  }
+#endif
+
+#endif /* __PATTERNLISTS__ */
+
+/* end of file */
